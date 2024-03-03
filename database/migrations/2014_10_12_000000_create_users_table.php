0abca4693fee27f6afba78a52642ac8ee3d659db
dhermawan215 <dhermawan215@gmail.com>
2024-03-03 22:31:41 +0700

    feat(database design system part 1)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('roles')->nullable();
+            $table->tinyInteger('is_active')->default('0');
             $table->rememberToken();
             $table->timestamps();
         });
