c764f4433ffb05f71af4e2030865d255bbd9fa46
dhermawan215 <dhermawan215@gmail.com>
2024-06-01 18:23:53 +0700

    feat(help desk category)

---
+++ app/Http/Controllers/HelpDeskCategoryController.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helper\SysMenu;
+use Illuminate\Http\Request;
+use App\Models\HelpDeskCategory;
+use Illuminate\Support\Facades\Validator;
+
+class HelpDeskCategoryController extends Controller
+{
+    /**
+     * controller for module help desk category
+     */
+    protected const sysModuleName = 'help_desk_category';
+    protected const title = 'Help Desk Category';
+    public static $url;
+
+    public function __construct()
+    {
+        static::$url = \route('help_desk_category');
+    }
+
+    private function modulePermission()
+    {
+        return SysMenu::menuSetingPermission(static::sysModuleName);
+    }
+    /**
+     * this method used for handle view when user access route /help-desk-category
+     * @return view 
+     */
+    public function index()
+    {
+        $modulePermission = $this->modulePermission();
+        if (!isset($modulePermission->is_access)) {
+            return \abort('403');
+        }
+        $moduleFn = \json_decode($modulePermission->function, true);
+        return \view('pages.help-desk-category.index', [
+            'title' => static::title,
+            'moduleFn' => $moduleFn
+        ]);
+    }
+    /**
+     * @method handle datatable help desk category
+     * @return json
+     */
+    public function list(Request $request)
+    {
+        $draw = $request['draw'];
+        $offset = $request['start'] ? $request['start'] : 0;
+        $limit = $request['length'] ? $request['length'] : 15;
+        $globalSearch = $request['search']['value'];
+
+        $query = HelpDeskCategory::select('id', 'category_name');
+
+        if ($globalSearch) {
+            $query->where('category_name', 'like', '%' . $globalSearch . '%');
+        }
+        $recordsFiltered = $query->count();
+
+        $resData = $query->skip($offset)
+            ->take($limit)
+            ->get();
+
+        $recordsTotal = $resData->count();
+
+        $data = [];
+        $i = $offset + 1;
+        $arr = [];
+        foreach ($resData as $key => $value) {
+            $data['cbox'] = '<input type="checkbox" class="data-menu-cbox" value="' . $value->id . '">';
+            $data['rnum'] = $i;
+            $data['name'] = $value->category_name;
+            $data['action'] = '<button class="btn btn-sm btn-primary btn-edit" data-edit="' . \base64_encode($value->id) . '" data-toggle="modal"
+            data-target="#modal-edit-category"><i class="fa fa-edit"></i></button>';
+            $arr[] = $data;
+            $i++;
+        }
+
+        return \response()->json([
+            'draw' => $draw,
+            'recordsTotal' => $recordsTotal,
+            'recordsFiltered' => $recordsFiltered,
+            'data' => $arr,
+        ]);
+    }
+    /**
+     * @method hanle request save help desk category
+     * @return json
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'category_name' => 'required|max:200'
+        ]);
+
+        if ($validator->fails()) {
+            return \response()->json($validator->errors(), 403);
+        }
+
+        HelpDeskCategory::create([
+            'category_name' => $request->category_name
+        ]);
+
+        return \response()->json(['success' => true, 'message' => 'Data saved'], 200);
+    }
+    /**
+     * @method hanle request update help desk category
+     * @param id encrypt
+     * @return json
+     */
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'category_name' => 'required|max:200'
+        ]);
+
+        if ($validator->fails()) {
+            return \response()->json($validator->errors(), 403);
+        }
+
+        $dataHelpDeskCategory = HelpDeskCategory::find(\base64_decode($id));
+
+        $dataHelpDeskCategory->update([
+            'category_name' => $request->category_name
+        ]);
+
+        return \response()->json(['success' => true, 'message' => 'Update success'], 200);
+    }
+    /**
+     * @method handle delete data
+     * @param multiple/single id
+     * @return json
+     */
+    public function destroy(Request $request)
+    {
+        HelpDeskCategory::whereIn('id', $request->dValue)->delete();
+        return \response()->json(['success' => true, 'message' => 'Delete success'], 200);
+    }
+}
---
+++ resources/views/pages/help-desk-category/index.blade.php
@@ -0,0 +1,138 @@
+@extends('layouts.app')
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">User Group Management</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Help Desk Category</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <button type="button" class="btn btn-sm btn-info" id="btn-refresh"><i
+                                    class="bi bi-arrow-clockwise"></i> Refresh</button>
+                            <button type="button" id="btn-add" class="btn btn-sm btn-primary" data-toggle="modal"
+                                data-target="#modal-add-category"><i class="fa fa-plus" aria-hidden="true"></i> Add
+                                Data</button>
+                            <button type="button" id="btn-delete" class="btn btn-sm btn-danger"><i class="fa fa-trash"
+                                    aria-hidden="true"></i>
+                                Delete</button>
+                        </div>
+                        <div class="card-body">
+                            <table id="table-help-desk-category" class="table table-bordered table-hover"
+                                style="width: 100%">
+                                <thead class="bg-primary">
+                                    <tr>
+                                        <th style="width: 15px">#</th>
+                                        <th style="width: 15px">No</th>
+                                        <th>Name</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.row (main row) -->
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+
+    <!-- modal help desk category -->
+    <div class="modal fade" id="modal-add-category">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Modal add help desk category</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form action="javascript:;" id="form-add-category" method="post">
+                    @csrf
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="category-name">Category Name</label>
+                            <input type="text" name="category_name" id="category-name" class="form-control"
+                                placeholder="category name">
+                        </div>
+                    </div>
+                    <div class="modal-footer justify-content-between">
+                        <button type="submit" class="btn btn-primary">Save</button>
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </form>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+    <!-- /modal add help desk category  -->
+
+    <!-- modal edit help desk category -->
+    <div class="modal fade" id="modal-edit-category">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Modal edit help desk category</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form action="javascript:;" id="form-edit-category" method="post">
+                    @csrf
+                    @method('PATCH')
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="category-name">Category Name</label>
+                            <input type="text" name="category_name" id="edit-category-name" class="form-control"
+                                placeholder="category name">
+                        </div>
+                    </div>
+                    <div class="modal-footer justify-content-between">
+                        <button type="submit" class="btn btn-success" id="btn-update">Update</button>
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </form>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+    <!-- /modal edit help desk category  -->
+@endsection
+@push('js-custom')
+    <script src="{{ asset('theme/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/jszip/jszip.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('theme/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+    <script>
+        ModuleFn = @json($moduleFn)
+    </script>
+    <script src="{{ asset('dist/js/pages/help-desk-category/view.min.js?q=') . time() }}"></script>
+@endpush
